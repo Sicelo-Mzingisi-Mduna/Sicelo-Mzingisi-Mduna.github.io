df373f6136097b9679dcda22e6392405e347d642
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-02-15 14:30:04 +0200

    fixed the nav bar to adjust for smaller screen sizes

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,13 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const navbarItems = document.querySelectorAll('.navbar ul li');
+    const navbarItems = document.querySelectorAll('.navbar ul li');
 
-  navbarItems.forEach(item => {
-      item.addEventListener('click', function() {
-          navbarItems.forEach(item => {
-              item.classList.remove('active');
-          });
-          this.classList.add('active');
-      });
-  });
+    navbarItems.forEach(item => {
+        item.addEventListener('click', function() {
+            // Check if the menu is currently shown
+            const checkbox = document.getElementById('check');
+            if (checkbox.checked) {
+                // Toggle the 'active' class
+                navbarItems.forEach(item => {
+                    item.classList.remove('active');
+                });
+                this.classList.add('active');
+            }
+        });
+    });
+
+    const checkbox = document.getElementById('check');
+    const dropdown = document.querySelector('.navbar ul');
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            dropdown.classList.add('show');
+        } else {
+            dropdown.classList.remove('show');
+        }
+    });
 });
+
+
+  
+  
 
