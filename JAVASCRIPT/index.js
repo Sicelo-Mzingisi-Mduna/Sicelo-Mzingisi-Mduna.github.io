ccf8137a5168380e2da8b5035c581d9e6a4ee41d
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-02-15 19:58:11 +0200

    implimented the responsive changes changes across all the webpages
    This means that all the web pages will display a mini nav area bar, when the pages are minimized.
    
    I also deleted the contents of index.js as i no longer needed it.

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,30 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarItems = document.querySelectorAll('.navbar ul li');
 
-    navbarItems.forEach(item => {
-        item.addEventListener('click', function() {
-            // Check if the menu is currently shown
-            const checkbox = document.getElementById('check');
-            if (checkbox.checked) {
-                // Toggle the 'active' class
-                navbarItems.forEach(item => {
-                    item.classList.remove('active');
-                });
-                this.classList.add('active');
-            }
-        });
-    });
-
-    const checkbox = document.getElementById('check');
-    const dropdown = document.querySelector('.navbar ul');
-    checkbox.addEventListener('change', function() {
-        if (this.checked) {
-            dropdown.classList.add('show');
-        } else {
-            dropdown.classList.remove('show');
-        }
-    });
-});
 
 
   
