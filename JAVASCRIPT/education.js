0a6de8cd4aa6a85420fbd77d94364cd316e7d0ca
Sicelo Mduna <122819041+Sicelo-Mzingisi-Mduna@users.noreply.github.com>
2024-02-16 18:22:19 +0200

    made changes to the nav area of each file
    1) added a on click java script effect, when the user hovers over an item in the nav bar, the item will be highlighted in white, so the user can track their location
    
    2) when the user clicks on an item in the nav area, the item will change to a light red color, this allows the user to know that the nav area is active and it also allows them to keep track of which item in the nav area they clicked on

---
+++ JAVASCRIPT/education.js
@@ -0,0 +1,14 @@
+const navItems = document.querySelectorAll('.navbar ul li');
+
+// Add click event listener to each list item
+navItems.forEach(item => {
+  item.addEventListener('click', function() {
+    // Remove the active class from all list items
+    navItems.forEach(item => {
+      item.classList.remove('active');
+    });
+
+    // Add the active class to the clicked item
+    this.classList.add('active');
+  });
+});
--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,5 +1,17 @@
+const navItems = document.querySelectorAll('.navbar-fieldset ul li');
 
+  // Add click event listener to each list item
+  navItems.forEach(item => {
+    item.addEventListener('click', function() {
+      // Remove the active class from all list items
+      navItems.forEach(item => {
+        item.classList.remove('active');
+      });
 
+      // Add the active class to the clicked item
+      this.classList.add('active');
+    });
+  });
 
   
   
